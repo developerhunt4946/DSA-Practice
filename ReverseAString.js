1aa5d0233436087c8e9dfea483e819edaac143b2
AyushGupta4946 <aagm0404@gmail.com>
2025-12-19 11:36:22 +0530

    question 1 done reverse a string using loop

---
+++ ReverseAString.js
@@ -0,0 +1,15 @@
+// Given a string, return the string reversed.
+// Input -- "hello"
+// Output -- "olleh"
+
+function reversedString(str) {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return reversed;
+}
+
+console.log(reversedString("Ayush"));
